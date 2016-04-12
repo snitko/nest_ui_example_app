ea6762fa508e92ab2b002a04b95309bbf875e4db
Roman Snitko <roman.snitko@gmail.com>
2016-04-13 00:47:46 +0300

    Make button disappear, wow!

--- app.dart
+++ app.dart
@@ -1,6 +1,19 @@
-import '../very_ui/lib/very_ui.dart';
+import 'package:nest_ui/nest_ui.dart';
+import 'dart:html';
+
+class Button extends Component {
+  List native_events = ["click"];
+  Button() {
+    event_handlers.add(role: #self, event: 'click', handler: (self) => disappear());
+    print(event_handlers.map);
+  }
+
+  disappear() {
+    dom_element.style..opacity=0; 
+  }
+}
 
 main() {
-  print("hello world");
-  assert(1==2);
+  var button = new Button();
+  button.dom_element = querySelector("button");
 }

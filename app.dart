84c1518229cd955c767eb964c4ee9055cc2acae0
Roman Snitko <roman.snitko@gmail.com>
2016-04-17 06:24:01 +0300

    Changing html_element text when changing Component property example

--- app.dart
+++ app.dart
@@ -1,14 +1,33 @@
 import 'package:nest_ui/nest_ui.dart';
 import 'dart:html';
 
-class Button extends Component {
-  List native_events = ["click"];
-  Button() {
-    event_handlers.add(event: 'click', handler: (self) => behave('hide'));
+class ButtonComponentBehaviors {
+
+  Component component;
+
+  ButtonComponentBehaviors() {}
+
+  clicked_caption_toggle() {
+    if(this.component.attributes['caption'] == 'Click me!')
+      this.component.caption = 'Clicked!';
+    else
+      this.component.caption = 'Click me!';
   }
+
+}
+
+class Button extends Component {
+  final List attribute_names = ["caption"];
+  List native_events   = ["click"];
+  List behaviors  = [ButtonComponentBehaviors];
+  Button() : super() { // we might not need that call to super(), not sure yet
+    event_handlers.add(event: 'click', handler: (self) => behave('clicked_caption_toggle'));
+  }
+
 }
 
 main() {
   var button = new Button();
   button.dom_element = querySelector("button");
+  print(button.caption);
 }

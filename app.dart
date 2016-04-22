03d77e6af07180bcc27ed9db5e771e13da1f6015
Roman Snitko <roman.snitko@gmail.com>
2016-04-22 19:36:31 +0300

    Demo: adding comments from the form

--- app.dart
+++ app.dart
@@ -1,33 +1,41 @@
 import 'package:nest_ui/nest_ui.dart';
 import 'dart:html';
 
-class ButtonComponentBehaviors {
-
-  Component component;
-
-  ButtonComponentBehaviors() {}
-
-  clicked_caption_toggle() {
-    if(this.component.attributes['caption'] == 'Click me!')
-      this.component.caption = 'Clicked!';
-    else
-      this.component.caption = 'Click me!';
-  }
-
+class RootComponent extends Component {
 }
 
-class Button extends Component {
+class ButtonComponent extends Component {
   final List attribute_names = ["caption"];
-  List native_events   = ["click"];
-  List behaviors  = [ButtonComponentBehaviors];
-  Button() : super() { // we might not need that call to super(), not sure yet
-    event_handlers.add(event: 'click', handler: (self) => behave('clicked_caption_toggle'));
+  List native_events = ["click"];
+  ButtonComponent() : super () {
+    event_handlers.add(event: 'click', role: #self, handler: (self) {
+      self.publishEvent("click");
+    });
   }
+}
 
+class CommentComponent extends Component {
+  final List attribute_names = ["body"];
+}
+
+class CommentFormComponent extends Component {
+  CommentFormComponent() : super() {
+    event_handlers.add(event: 'click', role: 'submit', handler: (self,p) {
+      var new_comment = new CommentComponent();
+      new_comment.body = self.dom_element.querySelector('textarea').value;
+      new_comment.initDomElementFromTemplate();
+      self.parent.addChild(new_comment);
+      self.parent.dom_element.append(new_comment.dom_element);
+    });
+  }
+}
+
+class TextAreaComponent extends Component {
+  final List attribute_names = ["text"];
 }
 
 main() {
-  var button = new Button();
-  button.dom_element = querySelector("button");
-  print(button.caption);
+  var root         = new RootComponent();
+  root.dom_element = querySelector('body');
+  root.initChildComponents();
 }

39f96718fe953661272d162f0f328bda37886fcb
Roman Snitko <roman.snitko@gmail.com>
2016-04-24 17:19:33 +0300

    Small changes for the compiled version

--- app.dart
+++ app.dart
@@ -7,11 +7,6 @@ class RootComponent extends Component {
 class ButtonComponent extends Component {
   final List attribute_names = ["caption"];
   List native_events = ["click"];
-  ButtonComponent() : super () {
-    event_handlers.add(event: 'click', role: #self, handler: (self) {
-      self.publishEvent("click");
-    });
-  }
 }
 
 class CommentComponent extends Component {
@@ -23,9 +18,7 @@ class CommentFormComponent extends Component {
     event_handlers.add(event: 'click', role: 'submit', handler: (self,p) {
       var new_comment = new CommentComponent();
       new_comment.body = self.dom_element.querySelector('textarea').value;
-      new_comment.initDomElementFromTemplate();
       self.parent.addChild(new_comment);
-      self.parent.dom_element.append(new_comment.dom_element);
     });
   }
 }
